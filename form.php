eceb59c567ebd767cef0d7b627685ea9b3ea1b70
musasibarani <persada.sibarani@gmail.com>
2017-09-28 17:02:12 +0700

    LEVEL 1
    Validating the Form Values

--- form.php
+++ form.php
@@ -1,12 +1,30 @@
 <?php
 if(isset($_POST['submit']))
-{ 
+{
+	$name = array("lawe tampan", "illing", "ruby", "aguero");
+	$min = 5;
+	$max = 10;
+	
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	
-	echo $username;
-	echo "<br/>";
-	echo $password;
+	if(strlen($username)<min)
+	{
+		echo "username harus memiliki panjang 5 atau lebih";
+	}
+	if(strlen($username)>max)
+	{
+		echo "username tidak boleh lebih panjang dari 10";
+	}
+	
+	if(!in_array($username, $name))
+	{
+		echo "maaf, akses anda gagal";
+	}
+	else
+	{
+		echo "selamat datang bous!!";
+	}
 }
 ?>
 
